f5504678200c86043c0479914916b6c92de922b5
yusufSLCN <yusuf.salcan@hotmail.com>
2023-07-02 13:46:57 +0200

    update dataset builder

--- dataset_builder.py
+++ dataset_builder.py
@@ -18,7 +18,7 @@ if __name__ == "__main__":
         os.mkdir(save_root_folder)
 
     class_ids = {'plane': '02691156', 'chair': '03001627', 'table': '04379243'}
-
+    discarded_samples = ['de45798ef57fe2d131b4f9e586a6d334']
     num_points = 16*16*16
     # num_points = 512
     threshold = 0.8
@@ -29,23 +29,35 @@ if __name__ == "__main__":
     with open(os.path.join(save_root_folder, 'dataset_info.json'), 'w') as fp:
         json.dump(dataset_info, fp)
 
+
+    tsdf_no = 0
     for cls_name in class_ids:
 
         class_save_folder = os.path.join(save_root_folder, cls_name)
         if not os.path.exists(class_save_folder):
             os.mkdir(class_save_folder)
         cls_path = os.path.join(root_folder, class_ids[cls_name])
-        for i, sample_id in enumerate(os.listdir(cls_path)):
-            sample_path = os.path.join(cls_path, sample_id + '/models/model_normalized.obj')
-            # try:
-            tsdf = obj_to_tsdf(sample_path, threshold, points3D)
-            # except FloatingPointError:
-            #     print('Cannot convert to sdf')
-            #     continue
+        for sample_id in os.listdir(cls_path):
 
-            tsdf_save_path = os.path.join(class_save_folder, f'{cls_name}_{i}.pkl')
+            sample_path = os.path.join(cls_path, sample_id + '/models/model_normalized.obj')
+            if not os.path.isfile(sample_path) or sample_id in discarded_samples:
+                print(f"Invalid file: {sample_path}")
+                continue
+        
+            tsdf_save_path = os.path.join(class_save_folder, f'{cls_name}_{tsdf_no}.pkl')
+            tsdf_no += 1
+            if os.path.exists(tsdf_save_path):
+                print(f"Already created: {tsdf_save_path}")
+                continue
+            else:
+                print(sample_path)
+
+            tsdf = obj_to_tsdf(sample_path, threshold, points3D)
+
             tsdf_sample = {'tsdf':tsdf, 'model_path':sample_path}
 
             with open(tsdf_save_path, 'wb') as f:
                 pickle.dump(tsdf_sample, f)
+
+            
         
