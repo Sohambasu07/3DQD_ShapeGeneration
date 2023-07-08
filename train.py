cd913313778b362c56cf8a4f42b4775fa8aa3a79
yusufSLCN <47846615+yusufSLCN@users.noreply.github.com>
2023-07-08 19:29:10 +0200

    add tensorboard logging

--- train.py
+++ train.py
@@ -8,31 +8,38 @@ from model.pvqvae.encoder import Encoder3D
 from model.pvqvae.decoder import Decoder3D
 from model.pvqvae.vqvae import VQVAE
 from utils import shape2patch
-
+from torch.utils.tensorboard import SummaryWriter
 
 # Training loop
-def train(model, dataloader, criterion, optimizer, device):
+def train(model, dataloader, criterion, optimizer, num_epoch=5,  device='cuda'):
+    writer = SummaryWriter()
+
     model.train()  # Set the model to train mode
+    for epeoch in range(num_epoch):
+        for batch_idx, tsdf_sample in enumerate(dataloader):
+            model_path = tsdf_sample[1][0]
+            tsdf = tsdf_sample[0][0]
 
-    for batch_idx, tsdf_sample in enumerate(dataloader):
-        model_path = tsdf_sample[1][0]
-        tsdf = tsdf_sample[0][0]
+            optimizer.zero_grad()  # Clear the gradients
 
-        optimizer.zero_grad()  # Clear the gradients
+            tsdf = tsdf.to(device)
+            tsdf = torch.reshape(tsdf, (1, 1, *tsdf.shape))
+            patched_tsdf = shape2patch(tsdf)
+            reconstructed_data, vq_loss = model(patched_tsdf)  # Forward pass
+            recon_loss = criterion(reconstructed_data, patched_tsdf)  # Compute the loss
 
-        tsdf = tsdf.to(device)
-        tsdf = torch.reshape(tsdf, (1, 1, *tsdf.shape))
-        patched_tsdf = shape2patch(tsdf)
-        reconstructed_data, vq_loss = model(patched_tsdf)  # Forward pass
-        recon_loss = criterion(reconstructed_data, patched_tsdf)  # Compute the loss
+            total_loss = recon_loss + vq_loss
+            total_loss.backward()  # Backpropagation
+            optimizer.step()  # Update the weights
 
-        total_loss = recon_loss + vq_loss
-        total_loss.backward()  # Backpropagation
-        optimizer.step()  # Update the weights
+            if batch_idx % 50 == 0:
+                iter_no = epeoch * len(data_loader) + batch_idx
+                print(f"Batch {iter_no}/{len(dataloader)} Total Loss: {total_loss.item()} Recon Loss: {recon_loss.item()}, Vq Loss: {vq_loss.item()}")
+                writer.add_scalar('Total loss/Train', total_loss.item(), iter_no)
+                writer.add_scalar('Recon loss/Train', recon_loss.item(), iter_no)
+                writer.add_scalar('VQ loss/Train', vq_loss.item(), iter_no)
+    writer.close()
 
-        if batch_idx % 10 == 0:
-            print(f"Batch {batch_idx}/{len(dataloader)} Total Loss: {total_loss.item()} Recon Loss: {recon_loss.item()}, Vq Loss: {vq_loss.item()}")
-            
 if __name__ == '__main__':
     shapenet_dataset = ShapeNet(r'./dataset')
     data_loader = DataLoader(shapenet_dataset, batch_size=1, shuffle=True)
@@ -62,6 +69,7 @@ if __name__ == '__main__':
 
     criterion = nn.MSELoss()
     optimizer = optim.Adam(model.parameters(), lr=0.001)
+    epoch = 5
 
-    train(model, data_loader, criterion, optimizer, device)
+    train(model, data_loader, criterion, optimizer, epoch, device)
     
