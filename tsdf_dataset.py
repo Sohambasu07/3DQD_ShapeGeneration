5ed3eaf68080b22d461b6819943442f1533071f0
Soham Basu <soham.basu07@gmail.com>
2023-07-10 12:29:29 +0200

    cleaned code
    Removed ShapeNet_viz stuff

--- tsdf_dataset.py
+++ tsdf_dataset.py
@@ -1,7 +1,4 @@
-import numpy as np
 import trimesh
-import math
-from utils import createCmap, viz_trimesh
 import pickle
 import os
 import json
@@ -55,21 +52,6 @@ if __name__ == '__main__':
     print(model_path)
     print(tsdf.shape)
     
-
-
-    # num_points = 512
-
-    # mesh = trimesh.load_mesh(model_path)
-    # meshes = mesh.dump(concatenate=True)
-    # merged_mesh = trimesh.util.concatenate(meshes)
-    # merged_mesh.show()
-
-    # grid = np.linspace(-1, 1, math.ceil(num_points**(1/3)))
-    # points3D = np.array([np.array([x, y, z]) for x in grid for y in grid for z in grid])
-
-    # colormap = createCmap(tsdf)
-
-    # viz_trimesh(merged_mesh, points3D, tsdf, colormap)
 
     tsdf = tsdf.numpy()
     vertices, faces, normals, _ = skimage.measure.marching_cubes(tsdf, level=0)
