57a721de3b713acf8d22f2a636ab8bfb2aa1506e
Muhammad Ali <m_ali98@outlook.com>
2023-07-15 14:39:00 +0200

    Use euclidean distance instead of dot product similarity

--- model/pvqvae/vector_quantizer.py
+++ model/pvqvae/vector_quantizer.py
@@ -1,5 +1,6 @@
 import torch
 import torch.nn as nn
+from einops import rearrange
 
 
 class VectorQuantizer(nn.Module):
@@ -8,7 +9,8 @@ class VectorQuantizer(nn.Module):
         self.n_embed = n_embed
         self.e_dim = e_dim
         self.beta = beta
-        self.codebook_hist = torch.zeros((self.n_embed)).to("cuda")
+        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        self.codebook_hist = torch.zeros(self.n_embed).to(self.device)
 
         # Create a lookup table with n_embed items, each of size e_dim
         self.embedding = nn.Embedding(self.n_embed, self.e_dim)
@@ -16,25 +18,26 @@ class VectorQuantizer(nn.Module):
     def forward(self, z):
         z_flattened = z.view(-1, self.e_dim)
 
-        # # distances from z to embeddings e_j (z - e)^2 = z^2 + e^2 - 2 e * z
-        # d = torch.sum(z_flattened ** 2, dim=1, keepdim=True) + \
-        #     torch.sum(self.embedding.weight ** 2, dim=1) - 2 * \
-        #     torch.einsum('bd,dn->bn', z_flattened, rearrange(self.embedding.weight, 'n d -> d n'))
-        # Don't understand this in the paper implementation. Is this the correct way?
-
         # Get all embeddings
-        indices = torch.arange(self.n_embed).to('cuda')
+        indices = torch.arange(self.n_embed).to(self.device)
         embeddings = self.embedding(indices)
 
         # Calculate dot product similarity between z and embeddings
-        similarity = torch.mm(z_flattened, embeddings.t())
+        similarity_old = torch.mm(z_flattened, embeddings.t())
+
+        # distances from z to embeddings e_j (z - e)^2 = z^2 + e^2 - 2 e * z
+        similarity = torch.sum(z_flattened ** 2, dim=1, keepdim=True) + \
+            torch.sum(self.embedding.weight ** 2, dim=1) - 2 * \
+            torch.einsum('bd,dn->bn', z_flattened, rearrange(self.embedding.weight, 'n d -> d n'))
+        # Don't understand this in the paper implementation. Is this the correct way?
+        # print(f'{similarity_old[0]}\n{similarity[0]}')
 
         codebook_idxs = torch.argmax(similarity, dim=-1)
         self.codebook_hist[codebook_idxs] += 1
         z_q = self.embedding(codebook_idxs).view(z.shape)
 
         # Third term in the loss but implemented differently from what is in the paper
-        vq_loss = torch.mean((z_q - z.detach()) ** 2) 
+        vq_loss = torch.mean((z_q - z.detach()) ** 2)
         commitment_loss = self.beta * torch.mean((z_q.detach() - z) ** 2)
 
         # preserve gradients
